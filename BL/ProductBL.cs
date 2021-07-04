d07236d63f29e24138fa9dfc40e6f3f7df3d6720
Julian Perez Demonty <jperezdemonty@live.com.ar>
2021-07-04 20:54:19 -0300

    Lista de productos y efectos

---
+++ BL/ProductBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+
+namespace BL
+{
+    public class ProductBL
+    {
+        public List<Product> GetProducts()
+        {
+            var db = new DAL.BaseDataService<Product>();
+            return db.Get();
+        }
+    }
+}
