98ef653fabd69f09e22b88aa3fbe37eb6cc287f2
FedeGA2801 <federico2801@gmail.com>
2021-07-13 19:14:28 -0300

    mejora

---
+++ DAL/Migrations/202107130241359_InitialCreate1.cs
@@ -0,0 +1,18 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Product", "ProductImage", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Product", "ProductImage");
+        }
+    }
+}
